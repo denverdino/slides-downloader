d9233ec9d0d732f893623c7f09e9979500010180
Li Yi <denverdino@gmail.com>
2020-08-18 09:45:52 +0800

    Support multiple attachement downloading

--- downloader.sh
+++ downloader.sh
@@ -9,15 +9,23 @@ for DAY in "${DAYS[@]}"; do
   LINKS=($(curl -s ${SCHED_URL}/${DAY}/overview | grep -oEi "f='(.*)' cl" | cut -d\' -f 2 | tr '\n' ' '))
   for LINK in "${LINKS[@]}"; do
     echo "Requesting ${SCHED_URL}/${LINK}"
-    FILE_URL=$(curl -s ${SCHED_URL}/${LINK} | grep "file-uploaded" | cut -d\" -f 4)
-    if [ -n "${FILE_URL}" ]; then
-      FILEPATH="${EVENT}/${DAY}/$(echo "${LINK}" | cut -d/ -f3).${FILE_URL##*.}"
-      if [[ ! -f "${FILEPATH}" ]]; then
-        echo "Downloading ${FILEPATH}"
-        curl -o "${FILEPATH}" -s "${FILE_URL}"
-      else
-        echo "Skip downloaded file ${FILEPATH}"
-      fi
+    FILE_URLS=$(curl -s ${SCHED_URL}/${LINK} | grep "file-uploaded" | cut -d\" -f 4)
+    if [ -n "${FILE_URLS}" ]; then
+      ITER=0
+      for FILE_URL in $FILE_URLS; do
+        if [ "$ITER" -eq "0" ]; then
+          FILEPATH="${EVENT}/${DAY}/$(echo "${LINK}" | cut -d/ -f3).${FILE_URL##*.}"
+        else
+          FILEPATH="${EVENT}/${DAY}/$(echo "${LINK}" | cut -d/ -f3)-${ITER}.${FILE_URL##*.}"
+        fi
+        if [[ ! -f "${FILEPATH}" ]]; then
+          echo "Downloading ${FILEPATH} from ${FILE_URL}"
+          curl --retry 3 --retry-delay 3 -o "${FILEPATH}" -s "${FILE_URL}"
+        else
+          echo "Skip downloaded file ${FILEPATH}"
+        fi
+        ITER=$(expr $ITER + 1)
+      done
     fi
   done
 done
