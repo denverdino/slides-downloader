704c0486aa7dfd616ba7b03322051e158d19700e
Li Yi <denverdino@gmail.com>
2020-11-06 09:31:26 +0800

    Add EnvoyCon and cdCon 2020

---
+++ cdcon2020.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download Continuous Delivery Foundation cdCon 2020 presentations from Sched
+
+DAYS=("2020-10-07")
+EVENT=cdcon2020
+
+source ./downloader.sh
---
+++ envoycon2020.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download EnvoyCon 2020 presentations from Sched
+
+DAYS=("2020-10-15")
+EVENT=envoycon2020
+
+source ./downloader.sh
