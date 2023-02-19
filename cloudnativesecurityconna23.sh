314084061acedc6573c8fd7efe66a6fb9255a131
denverdino <denverdino@gmail.com>
2023-02-19 08:58:30 +0800

    Update date of CloudNativeSecurityCon 2023 NA

--- cloudnativesecurityconna23.sh
+++ cloudnativesecurityconna23.sh
@@ -2,7 +2,7 @@
 
 # Download CloudNativeSecurityCon 2022 presentations from Sched
 
-DAYS=("2023-02-01" "2022-02-02")
+DAYS=("2023-02-01" "2023-02-02")
 EVENT=cloudnativesecurityconna23
 
 source ./downloader.sh
