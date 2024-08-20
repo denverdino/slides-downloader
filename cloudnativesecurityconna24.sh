5c123417998ee6da689f3260f14a8484be1f4e8c
denverdino <denverdino@gmail.com>
2024-08-20 17:59:56 +0800

    Add KubeCon + CloudNativeCon + Open Source Summit + AI_dev China 2024

---
+++ cloudnativesecurityconna24.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download CloudNativeSecurityCon North America 2024 presentations from Sched
+
+DAYS=("2024-06-26" "2022-06-27")
+EVENT=cloudnativesecurityconna24
+
+source ./downloader.sh
---
+++ kccncossaidevchn2024.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download KubeCon + CloudNativeCon + Open Source Summit + AI_dev China 2024 presentations from Sched
+
+DAYS=("2024-08-21" "2022-08-22" "2022-08-23")
+EVENT=kccncossaidevchn2024
+
+source ./downloader.sh
