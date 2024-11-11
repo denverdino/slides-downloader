bc82c2f9fb37438330f801d9bd58e76e0f22a278
denverdino <denverdino@gmail.com>
2024-11-11 09:34:20 +0800

    KubeCon + CloudNativeCon North America 2024

--- kccncna2023.sh
+++ kccncna2023.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Download KubeCon + CloudNativeCon North America 2022 presentations from Sched
+# Download KubeCon + CloudNativeCon North America 2023 presentations from Sched
 
 DAYS=("2023-11-06" "2023-11-07" "2023-11-08" "2023-11-09")
 EVENT=kccncna2023
---
+++ kccncna2024.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download KubeCon + CloudNativeCon North America 2024 presentations from Sched
+
+DAYS=("2024-11-12" "2024-11-13" "2024-11-14" "2024-11-15")
+EVENT=kccncna2024
+
+source ./downloader.sh
