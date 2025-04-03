07ebe4798a6f26bd668bacfb86ff53502a64d3e9
denverdino <denverdino@gmail.com>
2025-04-03 21:46:57 +0800

    Add KubeCon EU 2025

---
+++ kccnceu2025.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download KubeCon + CloudNativeCon Europe 2025 presentations from Sched
+
+DAYS=("2025-04-01" "2025-04-02" "2025-04-03" "2025-04-04")
+EVENT=kccnceu2025
+
+source ./downloader.sh
---
+++ wasmcon24.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download WASMCon 2024 presentations from Sched
+
+DAYS=("2024-11-11" "2024-11-12")
+EVENT=wasmcon24
+
+source ./downloader.sh
