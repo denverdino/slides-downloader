18e9622b4d1fe63078240f105b9a533dd701c1e4
zhaobo.zb <zhaobo.zb@bytedance.com>
2025-11-15 23:14:05 +0800

    Add KubeCon + CloudNativeCon North America 2025

---
+++ kccncna2025.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download KubeCon + CloudNativeCon North America 2025 presentations from Sched
+
+DAYS=("2025-11-10" "2025-11-11" "2025-11-12")
+EVENT=kccncna2025
+
+source ./downloader.sh
